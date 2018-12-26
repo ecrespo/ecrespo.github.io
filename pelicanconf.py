5d603c3a1719743c6728a9bd6508a51394f1acea
Ernesto Crespo <ecrespo@gmail.com>
2018-12-25 21:07:10 -0400

    incorporado configuraciones para flex

--- pelicanconf.py
+++ pelicanconf.py
@@ -12,6 +12,8 @@ TIMEZONE = 'America/Caracas'
 
 DEFAULT_LANG = 'es'
 
+MAIN_MENU = True
+
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
@@ -26,8 +28,23 @@ LINKS = (('Pelican', 'http://getpelican.com/'),
          ('You can modify those links in your config file', '#'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+#SOCIAL = (('You can add links in your config file', '#'),
+#          ('Another social link', '#'),)
+
+SOCIAL = (('linkedin', 'http://ve.linkedin.com/in/ernestocrespo'),
+          ('github', 'https://github.com/ecrespo'),
+          ('google', 'https://google.com/+ErnestoCrespo'),
+          ('twitter', 'https://twitter.com/_seraph1'),
+          ('facebook', 'https://www.facebook.com/ernesto.crespo'),
+          ('gitlab', 'https://gitlab.com/ecrespo'),
+          ('soundcloud','https://soundcloud.com/ernesto-crespo'),
+          ('rss', '//blog.alexandrevicenzi.com/feeds/all.atom.xml'))
+
+
+MENUITEMS = (('Archives', '/archives.html'),
+             ('Categories', '/categories.html'),
+             ('Tags', '/tags.html'),)
+
 
 DEFAULT_PAGINATION = 10
 
@@ -36,4 +53,6 @@ DEFAULT_PAGINATION = 10
 
 
 THEME='/home/ernesto/proyectos/ecrespo.github.io/themes/Flex'
+PYGMENTS_STYLE = 'monokai'
+
 

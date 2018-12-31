e82ed6bf0891779907749bb82bf7539d5c0325a6
Ernesto Crespo <ecrespo@gmail.com>
2018-12-31 13:17:54 -0400

    Arreglado warning

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 
 AUTHOR = 'Ernesto Crespo'
 SITENAME = "Página de Seraph"
-SITEURL = ''
+SITEURL = 'http://localhost:8000'
 SITETITLE = AUTHOR
 SITESUBTITLE = 'Data Science'
 SITEDESCRIPTION = ' Software Libre, Ciencia de Datos y Python'
@@ -16,12 +16,20 @@ BROWSER_COLOR = '#e5e5ff'
 TIMEZONE = 'America/Caracas'
 
 DEFAULT_LANG = 'es'
+I18N_TEMPLATES_LANG = 'es'
+DEFAULT_LANG = 'es'
+#OG_LOCALE = 'es_VE'
+#LOCALE = 'es_VE'
+
+DATE_FORMATS = {
+    'en': '%B %d, %Y',
+}
 
 MAIN_MENU = True
 
 # Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
-CATEGORY_FEED_ATOM = None
+FEED_ALL_ATOM = 'feeds/all.atom.xml'
+CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
@@ -30,7 +38,7 @@ AUTHOR_FEED_RSS = None
 LINKS = (('Pelican', 'http://getpelican.com/'),
          ('Python.org', 'http://python.org/'),
          ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+         )
 
 # Social widget
 #SOCIAL = (('You can add links in your config file', '#'),
